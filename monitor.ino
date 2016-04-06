36739478947b59ccaac6791dacaab6edfe8e6947
Travis Smith <travissmith94@hotmail.com>
2016-04-06 16:57:34 -0500

    Averages heart rate over a second

--- monitor.ino
+++ monitor.ino
@@ -2,19 +2,28 @@
 #define YELLOW 13
 #define RED 12
 #define BUZZER 11
+#define FSR 0
 
 //Alert levels
 #define SAFE 0
 #define WARN 1
 #define DANGER 2
 
+#define FAST 150
+#define NORMAL 80
+#define SLOW   30
 
 
+
+int level = SAFE;
+float heartRate = NORMAL;
+  
 void setup() {
+  Serial.begin(9600);
   pinMode(YELLOW , OUTPUT);
   pinMode(RED , OUTPUT);
   pinMode(BUZZER , OUTPUT);
-
+  pinMode(FSR , INPUT);
 }
 
 void alert(int level){
@@ -33,9 +42,18 @@ void alert(int level){
       break;
   }
 }
-void loop() {
-  //alert(SAFE);
-  //alert(WARN);
-  //alert(DANGER);
-  
+
+void loop() { 
+  float current = millis();
+  int beats = 0;
+  while(current + 1000 > millis()){
+    float reading = analogRead(FSR);
+    if(reading > 0){
+      beats++; 
+    }
+    delay(100);
+  }
+  heartRate +=  (beats * 60);
+  heartRate /= 2;
+  Serial.println(heartRate);
 }
